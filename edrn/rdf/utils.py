d616ba1eb1194a74060b24188f165b51c6c90119
Sean Kelly <sean.c.kelly@me.com>
2010-01-19 17:27:55 +0000

    Official EKE database host

--- edrn/rdf/utils.py
+++ edrn/rdf/utils.py
@@ -17,7 +17,7 @@ class RDFDatabase(object):
         return pymssql.connect(
             user='ekeuser',
             password='Hello999',
-            host='pongo.fhcrc.org:1433',
+            host='compass1.fhcrc.org:1433',
             database='dbEKE'
         )
     
