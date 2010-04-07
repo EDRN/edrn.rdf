3c5e7fb147cb56761ba3eba985fa99ace207b257
Sean Kelly <sean.c.kelly@me.com>
2010-04-07 22:17:08 +0000

    bumpity

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from setuptools import setup, find_packages
 import os
 
 _name = 'edrn.rdf'
-_version = '0.0.10'
+_version = '0.0.11'
 
 def _read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
