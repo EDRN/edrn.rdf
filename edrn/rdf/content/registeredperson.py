d346bbad66ed5694121467cfe012a8fa34f227e0
Sean Kelly <sean.c.kelly@me.com>
2010-12-08 21:50:27 +0000

    Remove all addresses from sites. Add address to people.

--- edrn/rdf/content/registeredperson.py
+++ edrn/rdf/content/registeredperson.py
@@ -114,6 +114,60 @@ RegisteredPersonSchema = base.SourceSchema.copy() + atapi.Schema((
             description=_(u'Uniform Resource Identifier for the account name predicate.'),
         ),
     ),
+    atapi.StringField(
+        'addr1URI',
+        required=True,
+        storage=atapi.AnnotationStorage(),
+        widget=atapi.StringWidget(
+            label=_(u'Mailing Address Line 1 URI'),
+            description=_(u'Uniform Resource Identifier for line 1 of the mailing address.'),
+        ),
+    ),
+    atapi.StringField(
+        'addr2URI',
+        required=True,
+        storage=atapi.AnnotationStorage(),
+        widget=atapi.StringWidget(
+            label=_(u'Mailing Address Line 2 URI'),
+            description=_(u'Uniform Resource Identifier for line 2 of the mailing address.'),
+        ),
+    ),
+    atapi.StringField(
+        'cityURI',
+        required=True,
+        storage=atapi.AnnotationStorage(),
+        widget=atapi.StringWidget(
+            label=_(u'Mailing Address City URI'),
+            description=_(u'Uniform Resource Identifier for the city of the mailing address predicate.'),
+        ),
+    ),
+    atapi.StringField(
+        'stateURI',
+        required=True,
+        storage=atapi.AnnotationStorage(),
+        widget=atapi.StringWidget(
+            label=_(u'Mailing Address State URI'),
+            description=_(u'Uniform Resource Identifier for the state of the mailing address predicate.'),
+        ),
+    ),
+    atapi.StringField(
+        'zipURI',
+        required=True,
+        storage=atapi.AnnotationStorage(),
+        widget=atapi.StringWidget(
+            label=_(u'Mailing Address Post Code URI'),
+            description=_(u'Uniform Resource Identifier for the post code (zip code) of the mailing address predicate.'),
+        ),
+    ),
+    atapi.StringField(
+        'countryURI',
+        required=True,
+        storage=atapi.AnnotationStorage(),
+        widget=atapi.StringWidget(
+            label=_(u'Mailing Address Country URI'),
+            description=_(u'Uniform Resource Identifier for the country of the mailing address predicate.'),
+        ),
+    ),
 ))
 
 finalizeATCTSchema(RegisteredPersonSchema, folderish=False, moveDiscussion=True)
@@ -135,5 +189,11 @@ class RegisteredPerson(base.Source):
     edrnTitleURI              = atapi.ATFieldProperty('edrnTitleURI')
     siteURI                   = atapi.ATFieldProperty('siteURI')
     userIDURI                 = atapi.ATFieldProperty('userIDURI')
+    addr1URI                  = atapi.ATFieldProperty('addr1URI')
+    addr2URI                  = atapi.ATFieldProperty('addr2URI')
+    cityURI                   = atapi.ATFieldProperty('cityURI')
+    stateURI                  = atapi.ATFieldProperty('stateURI')
+    zipURI                    = atapi.ATFieldProperty('zipURI')
+    countryURI                = atapi.ATFieldProperty('countryURI')
 
 atapi.registerType(RegisteredPerson, PROJECTNAME)
