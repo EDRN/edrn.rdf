71e2b23eb359438241c1b0b858e785df73690bbd
Sean Kelly <sean.c.kelly@me.com>
2012-09-20 03:22:34 +0000

    Make inactivity raise an exception; don't string compare RDF since isomorphic models can produce different serializations

--- edrn/rdf/rdfupdater.py
+++ edrn/rdf/rdfupdater.py
@@ -2,6 +2,8 @@
 # Copyright 2012 California Institute of Technology. ALL RIGHTS
 # RESERVED. U.S. Government Sponsorship acknowledged.
 
+from rdflib import Graph
+from rdflib.compare import isomorphic
 from Acquisition import aq_inner
 from edrn.rdf.interfaces import IRDFUpdater, IGraphGenerator
 from edrn.rdf.rdfsource import IRDFSource
@@ -11,7 +13,7 @@ from zope.component import getUtility
 from zope.event import notify
 from zope.lifecycleevent import ObjectModifiedEvent
 from five import grok
-from exceptions import NoGeneratorError, NoUpdateRequired
+from exceptions import NoGeneratorError, NoUpdateRequired, SourceNotActive
 import datetime
 
 RDF_XML_MIMETYPE = 'application/rdf+xml'
@@ -25,7 +27,8 @@ class RDFUpdater(grok.Adapter):
     def updateRDF(self):
         context = aq_inner(self.context)
         # If the RDF Source is inactive, we're done
-        if not context.active: return
+        if not context.active:
+            raise SourceNotActive(context)
         # Check if the RDF Source has an RDF Generator
         if not context.generator:
             raise NoGeneratorError(context)
@@ -34,20 +37,21 @@ class RDFUpdater(grok.Adapter):
         # Adapt the generator to a graph generator, and get the graph in XML form.
         generator = IGraphGenerator(generator)
         graph = generator.generateGraph()
-        serialized = graph.serialize()
         # Is there an active file?
         if context.approvedFile:
             # Is it identical to what we just generated?
-            current = context.approvedFile.to_object.get_data()
-            if serialized == current:
+            current = Graph().parse(data=context.approvedFile.to_object.get_data())
+            if isomorphic(graph, current):
                 raise NoUpdateRequired(context)
         # Create a new file and set it active
         # TODO: Add validation steps here
+        serialized = graph.serialize()
+        timestamp = datetime.datetime.utcnow().isoformat()
         newFile = context[context.invokeFactory(
             'File',
             context.generateUniqueId('File'),
-            title=u'RDF',
-            description=u'Generated at %s by %s' % (datetime.datetime.utcnow().isoformat(), generatorPath),
+            title=u'RDF %s' % timestamp,
+            description=u'Generated at %s by %s' % (timestamp, generatorPath),
             file=serialized,
         )]
         newFile.getFile().setContentType(RDF_XML_MIMETYPE)
